aef59ecc1978775ceb1d9918d5dadc73af77ac95
Ajitkumar-1001 <sachinajitkumarpr@gmail.com>
2026-04-08 23:57:17 -0400

    Hard problem2

---
+++ hard_problem2.py
@@ -0,0 +1,8 @@
+## XOR multiplication on queries on 2 
+from typing import List 
+
+class XORmultiplication:
+    def __init__(self, nums:List[int] , queries:List[List[int]]) -> int:
+
+
+        return 
